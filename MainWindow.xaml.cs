1464c4faf7b9a241908c5aec662b2c5fc46cd811
Palak Maheria <palak.maheria@hotmail.com>
2014-04-11 16:22:09 +0530

    Initial commit
    Pure xaml wpf analog clock

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAnalogClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.SecondHandLineRotateTransform.Angle = DateTime.Now.Second * 6;
+            this.MinuteHandLineRotateTransform.Angle = DateTime.Now.Minute * 6;
+            this.HourHandLineRotateTransform.Angle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5);
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
